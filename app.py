73c30c8736138c00eea337759e4b5d7cb43b298d
mpio <mpiotro4@outlook.com>
2025-12-06 20:43:12 +0100

    add config variables to app

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ import socket
 
 app = Flask(__name__)
 
+APP_PORT = int(os.getenv("APP_PORT", "8080"))
+APP_MESSAGE = os.getenv("APP_MESSAGE", "default message")
+
 @app.route("/")
 def info():
     data = {
         "status": "ok",
+        "message": APP_MESSAGE,
         "hostname": socket.gethostname(),
         "pod_ip": os.getenv("POD_IP"),
         "namespace": os.getenv("POD_NAMESPACE"),
@@ -16,4 +20,4 @@ def info():
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=APP_PORT)
